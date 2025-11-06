f04e8e7d385ab0700ad0248bc94751b8766d396a
devanandanmcyeah <devanandanmcyeah@gmail.com>
2025-11-06 11:49:00 +0530

    Add files via upload

---
+++ Stroing in arrays.php
@@ -0,0 +1,17 @@
+<?php
+$students = array("CRISTIANO", "RAMOS", "BALE", "BENZIMA", "MARCELO");
+echo "<h3>Original Student List:</h3>";
+echo "<pre>";
+print_r($students);
+echo "</pre>";
+asort($students);
+echo "<h3>Sorted Student List (Ascending):</h3>";
+echo "<pre>";
+print_r($students);
+echo "</pre>";
+arsort($students);
+echo "<h3>Sorted Student List (Descending):</h3>";
+echo "<pre>";
+print_r($students);
+echo "</pre>";
+?>
